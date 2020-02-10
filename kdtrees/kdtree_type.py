92ab0d85415a180ee72798b8786de941d7033515
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-02-10 17:20:10 -0500

    Setup required functions

--- kdtrees/kdtree_type.py
+++ kdtrees/kdtree_type.py
@@ -1,4 +1,27 @@
 from abc import ABC, abstractmethod
 
-class KDTreeType(ABC):
-	 
+class KDTreeType(ABC, list):
+
+	def __init__(self):
+		pass
+
+	def __eq__(self, other):
+		if isinstance(other, self.__class__):
+			return self.__dict__ == other.__dict__
+		else:
+			return False
+
+	def __ne__(self, other):
+		return not self.__eq__(other)
+
+	@abstractmethod
+	def __len__(self, key):
+		raise NotImplementedError("__len__ not implemented")
+
+	@abstractmethod
+	def __getitem__(self, key):
+		raise NotImplementedError("__getitem__ not implemented")
+
+	@abstractmethod
+	def __iter__(self):
+		raise NotImplementedError("__iter__ not implemented")
