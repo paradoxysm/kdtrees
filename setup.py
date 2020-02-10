87cfb8c356bf183ca26a20457766b48e6ed205ed
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-02-10 00:52:56 -0500

    Add 2.7 to python_requires

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 	extras_require={
         'test': ['pytest', 'coverage', 'pytest-cov']
     },
-	python_requires='>=3.4, <4',
+	python_requires='>= 2.7, <4',
 	classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
