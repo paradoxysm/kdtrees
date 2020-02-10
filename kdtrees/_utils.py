4998d15d10fd6dc02740421ff87890a16e7beb41
Jeffrey Wang <jeffreyc.wang@mail.utoronto.ca>
2020-02-09 21:55:06 -0500

    Implement format_array and check_dimensionality

--- kdtrees/_utils.py
+++ kdtrees/_utils.py
@@ -3,22 +3,33 @@
 # Authors: Jeffrey Wang
 # License: BSD 3 clause
 
-def format_arrays(*args):
+def format_array(arr, list=False):
 	"""
 	Format all arrays given into numpy ndarrays
 	such that the last axis denotes the features.
+	If the argument given is a scalar, wrap into a list first.
 
 	Parameters
 	----------
-	*args : tuple, default=()
-		Tuple of array-like objects to convert into ndarrays.
+	arr : array-like
+		array-like object to convert into ndarray.
 
 	Returns
 	-------
-	arrays : list
-		List of formatted ndarrays where the last axis denotes the features.
+	ndarr : ndarray
+		Formatted ndarrays where the last axis denotes the features.
 	"""
-	pass
+	if np.isscalar(arr):
+		return np.asarray([arr])
+	np_arr = np.asarray(arr)
+	if list:
+		if np_arr.ndim != np_arr.shape[-1]:
+			return np_arr[:, np.new_axis]
+		else:
+			return np_arr
+	else:
+		return np_arr = np.squeeze(np_arr)
+
 
 def check_dimensionality(*args):
 	"""
@@ -28,11 +39,23 @@ def check_dimensionality(*args):
 	Parameters
 	----------
 	*args : tuple, default=()
-		Tuple of array-like objects or scalars to evaluate.
+		Tuple of ndarrays where the last axis denotes the features.
 
 	Returns
 	-------
 	dim : int
 		The dimensionality of all given arguments.
 	"""
-	pass
+	if len(args) == 0:
+		raise ValueError("Must contain at least one argument")
+	if np.any(args == None):
+		raise ValueError("Must contain non-None arguments")
+	if np.any(not isinstance(args, np.ndarray)):
+		raise ValueError("Must contain ndarray arguments")
+	dim = None
+	for arg in args:
+		if dim is None:
+			dim = arg.shape[-1]
+		elif dim != arg.shape[-1]:
+			raise ValueError("Arguments must be the same dimensions")
+	return dim
